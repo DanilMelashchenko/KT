06ba8f262089d3b909b59f97fea3e9ba367dad21
DanilMelashchenko <d.melashchenko@htcolledge.sumdu.edu.ua>
2025-03-18 12:53:00 +0200

    задание1

---
+++ Work2.js
@@ -0,0 +1,14 @@
+let num1 = parseFloat(prompt("Введіть перше число"));
+let num2 = parseFloat(prompt("Введіть друге число"));
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+let remainder = num1 % num2;
+
+alert(`Сумма: ${sum}`);
+alert(`Різниця: ${difference}`);
+alert(`Добуток: ${product}`);
+alert(`Частка: ${quotient}`);
+alert(`Залишок від ділення: ${remainder}`);
