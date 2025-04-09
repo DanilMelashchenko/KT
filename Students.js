f0f0833fa2fa867e375ffa984973d343d97187a0
DanilMelashchenko <d.melashchenko@htcolledge.sumdu.edu.ua>
2025-04-09 14:11:33 +0300

    Students

--- Students.js
+++ Students.js
@@ -2,33 +2,39 @@ const student = {
     name: "Андрій",
     specialty: "Комп'ютерні науки",
     averageGrade: 98,
-    missedCkasses: 1,
+    missedClasses: 1,
 
     showInfo: function() {
-        console.log(`Ім'я: ${this.name}`);
-        console.log(`Спеціальність: ${this.specialty}`);
-        console.log(`Середній бал: ${this.averageGrade}`);
-        console.log(`Пропущенно занять: ${this.missedCkasses}`);
-        console.log(`----------------------------`);
+        return `
+      <strong>Ім'я:</strong> ${this.name}<br>
+      <strong>Спеціальність:</strong> ${this.specialty}<br>
+      <strong>Середній бал:</strong> ${this.averageGrade}<br>
+      <strong>Пропущено занять:</strong> ${this.missedClasses}<br><hr>
+    `;
     }
 };
 
 const student2 = {
     name: "Олена",
     specialty: "Веб Дизайн",
-    averageGrade: 88,
-    missedCkasses: 12
+    averageGrade: 98,
+    missedClasses: 12
 };
 
 const student3 = {
     name: "Ігор",
     specialty: "Механіка",
     averageGrade: 79,
-    missedCkasses: 5
+    missedClasses: 5
 };
 
-student.showInfo.call(student2);
-student.showInfo.apply(student3);
+function ShowAllStudents () {
+    const output = document.getElementById("output");
+    output.innerHTML = '';
 
-const showOriginalStudent = student.showInfo.bind(student);
-showOriginalStudent ();
+    output.innerHTML += student.showInfo.call(student2);
+    output.innerHTML += student.showInfo.apply(student3);
+
+    const ShowOriginal = student.showInfo.bind(student);
+    output.innerHTML += ShowOriginal();
+}
