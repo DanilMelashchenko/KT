fbf4c6cf651867690490a28cfd578277798498c6
DanilMelashchenko <d.melashchenko@htcolledge.sumdu.edu.ua>
2025-05-08 12:40:24 +0300

    PR9

---
+++ diya3.js
@@ -0,0 +1,11 @@
+const tree = document.getElementById("tree");
+tree.addEventListener("click", function (event) {
+    
+    const li = event.target.closest("li");
+    if (!li) return;
+    
+    const childUl = li.querySelector("ul");
+    if (childUl) {
+        childUl.hidden = !childUl.hidden;
+    }
+});
